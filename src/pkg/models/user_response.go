b1671b48fcb10af3b1e82dad57a86fa834aaa0ac
IvanParvanovski <ivan.parvanovski347@gmail.com>
2025-06-10 16:05:10 +0300

    Removed question ID; poll ID is sufficient for identification

--- src/pkg/models/user_response.go
+++ src/pkg/models/user_response.go
@@ -4,7 +4,6 @@ import "github.com/google/uuid"
 
 type UserResponse struct {
 	Id         uuid.UUID	`json:"id"`
-	QuestionId uuid.UUID	`json:"questionId"`
 	AnswerIds  []uuid.UUID	`json:"answerIds"`
 	PollId     uuid.UUID	`json:"pollId"`
 }
